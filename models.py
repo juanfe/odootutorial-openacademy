7541f7bfe254d8fff76728e7ed96803ff49f476e
Juan Fernando Jaramillo Botero <juanf.jaramillo@opdevel.com>
2015-02-02 15:50:57 -0500

    Create a session model

--- models.py
+++ models.py
@@ -9,3 +9,12 @@ class Course(osv.Model):
         'name' : fields.char('Title'),
         'description' : fields.text('Description'),
     }
+
+class Session(osv.Model):
+    _name = 'openacademy.session'
+    _columns = {
+        'name' : fields.char(required=True),
+        'start_date' : fields.date(default=fields.date.today),
+        'duration' : fields.float(digits=(6,2), help="Duration in days"),
+        'seats' : fields.integer(string="Number of seats"),
+    }
