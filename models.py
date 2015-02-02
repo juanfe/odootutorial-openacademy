af07efe03b5af84359007ada1f0b5b14e61284f1
Juan Fernando Jaramillo Botero <juanf.jaramillo@opdevel.com>
2015-02-02 15:17:00 -0500

    Module creation

---
+++ models.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from openerp import api
+from openerp.osv import fields, osv
+
+# class openacademy(models.Model):
+#     _name = 'openacademy.openacademy'
+
+#     name = fields.Char()
