55fe5a7dbd5e489215f767d634584b0d38713389
Juan Fernando Jaramillo Botero <juanf.jaramillo@opdevel.com>
2015-02-04 08:07:05 -0500

    Calendar view

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from datetime import timedelta
 from openerp import api, exceptions
 from openerp.osv import fields, osv
 
@@ -56,6 +57,8 @@ class Session(osv.Model):
         'attendee_ids' : fields.many2many('res.partner', string='Attendees'),
 
         'taken_seats' : fields.float(string="Taken seats", compute='_taken_seats'),
+        'end_date' : fields.date(string="End Date", store=True,
+                        compute='_get_end_date', inverse='_set_end_date'),
     }
 
     @api.one
@@ -82,6 +85,30 @@ class Session(osv.Model):
                     'message': "Increase seats or remove excess attendees",
                 },
             }
+
+    @api.one
+    @api.depends('start_date', 'duration')
+    def _get_end_date(self):
+        if not (self.start_date and self.duration):
+            self.end_date = self.start_date
+            return
+
+        # Add duration to start_date, but: Monday + 5 days = Saturday, so
+        # subtract one second to get on Friday instead
+        start = fields.Datetime.from_string(self.start_date)
+        duration = timedelta(days=self.duration, seconds=-1)
+        self.end_date = start + duration
+
+    @api.one
+    def _set_end_date(self):
+        if not (self.start_date and self.end_date):
+            return
+
+        # Compute the difference between dates, bu: Friday = 4 days,
+        # so add one day to get 5 days instead
+        start_date = fields.Datetime.from_string(self.start_date)
+        end_date = fields.Datetime.from_string(self.end_date)
+        self.duration = (end_date - start_date).days + 1
 
     @api.one
     @api.constrains('instructor_id', 'attendee_ids')
